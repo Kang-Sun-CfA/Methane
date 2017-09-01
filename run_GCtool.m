511c372dc4a85d0d55699dabf8ffe5181b93def5
Kang-Sun-CfA <franksun1016@gmail.com>
2017-08-31 21:06:12 -0400

    no idea

--- run_GCtool.m
+++ run_GCtool.m
@@ -14,11 +14,11 @@ inp760 = [];
 inp760.SZA = SZA;
 inp760.VZA = VZA;
 % wavelength, in nm
-inp760.vStart = 680; inp760.vEnd = 772; 
+inp760.vStart = 700; inp760.vEnd = 780; 
 % molecules to include
 inp760.gas_cell = {'O2','O4','H2O'};
 % spectral interval for calculation, in nm
-inp760.dv_calc = 0.015;
+inp760.dv_calc = 0.01;
 inp760.K1 = 0.1; inp760.K2 = 0.1; inp760.K3 = 0.00001;
 
 cd(GChome_dir)
